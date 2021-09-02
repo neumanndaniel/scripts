5ea27da4547731496078e0cc85f6bc55b658b88b
Daniel Neumann <13654904+neumanndaniel@users.noreply.github.com>
2021-09-02 23:21:16 +0200

    Optimize setup process

--- Bash/Podman/install.sh
+++ Bash/Podman/install.sh
@@ -9,9 +9,12 @@ brew install podman multipass
 ln -s /usr/local/bin/podman /usr/local/bin/docker || true
 
 # Podman setup
-SSH_PUB_KEY=$(cat ~/.ssh/id_rsa.pub)
-echo '
+SSH_CONFIG_EXISTS=$(cat user-data | grep -c "ssh_authorized_keys:")
+if [[ $SSH_CONFIG_EXISTS -eq 0 ]]; then
+  SSH_PUB_KEY=$(cat ~/.ssh/id_rsa.pub)
+  echo '
 ssh_authorized_keys:
   - '${SSH_PUB_KEY}'' >> user-data
+fi
 
 ./create.sh $PODMAN_MODE
