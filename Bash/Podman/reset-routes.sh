cca1db0512765c8fee96aefb65fb4b11507702f3
Daniel Neumann <13654904+neumanndaniel@users.noreply.github.com>
2021-12-06 23:51:13 +0100

    Update reset-routes.sh

--- Bash/Podman/reset-routes.sh
+++ Bash/Podman/reset-routes.sh
@@ -2,7 +2,6 @@
 
 INSTANCE_NAME="podman"
 
-multipass stop $INSTANCE_NAME
 sudo route -nv delete -net 192.168.64.0/24
-sudo route -nv add -net 192.168.64.0/24 -interface bridge100
-multipass start $INSTANCE_NAME
+sudo route -nv add -net 192.168.64.0/24 -interface bridge100 -static
+multipass restart $INSTANCE_NAME
